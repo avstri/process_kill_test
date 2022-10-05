9b04fab1d1be9426611c0fcd436115b66d3c1073
Alex Strigelskiy <alex.strigelskiy@ionpath.com>
2022-10-05 11:34:59 -0700

    checking synchronisity

--- pkill_test.py
+++ pkill_test.py
@@ -10,6 +10,9 @@ try:
         sleep(0.25)
 except KeyboardInterrupt:
     print("ok")
+    for x in range(10):
+        sleep(0.25)
+        print(x)
 finally:
     kill(nodeProc.pid, 2)
     print("gracefully finished, good bye")
